1db4c74426ae1868e0ff5209ac045124fde05ca8
Kulikova-A18 <81427431+Kulikova-A18@users.noreply.github.com>
2025-01-14 21:43:01 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,6 +1,20 @@
+p_c = '00110100'
+# '01010101', '11111111', '10101010'
+keys = ['11111111', '01010101', '10101010']
+
+# s_box = [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+# inverse_s_box = [14, 6, 12, 10, 5, 3, 15, 1, 0, 9, 4, 13, 7, 11, 2, 8]
+# p_indices = [1, 8, 3, 5, 7, 4, 6, 2]
+# inverse_p_indices = [8, 1, 5, 6, 4, 7, 2, 3]
+
+# s_box = [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7]
+# inverse_s_box = [14, 3, 4, 8, 1, 12, 10, 15, 7, 13, 9, 5, 11, 2, 0, 6]  # Обратный S-блок
+# p_indices = [1, 5, 2, 6, 3, 7, 4, 8]
+# inverse_p_indices = [1, 3, 5, 7, 2, 6, 4, 8]  # Обратный P-блок
+
 def s_box(input_bits):
     # Таблица S-блока
-    s_box = [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+    s_box = [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7]
     left = int(input_bits[:4], 2)  # Левые 4 бита
     right = int(input_bits[4:], 2)  # Правые 4 бита
     # Применяем S-блок к левым и правым частям
@@ -9,7 +23,7 @@ def s_box(input_bits):
     return output
 def inverse_s_box(output_bits):
     # Обратная таблица S-блока
-    inverse_s_box = [14, 6, 12, 10, 5, 3, 15, 1, 0, 9, 4, 13, 7, 11, 2, 8]
+    inverse_s_box = [14, 3, 4, 8, 1, 12, 10, 15, 7, 13, 9, 5, 11, 2, 0, 6]
     left = int(output_bits[:4], 2)  # Левые 4 бита
     right = int(output_bits[4:], 2)  # Правые 4 бита
     # Применяем обратный S-блок к левым и правым частям
@@ -18,13 +32,13 @@ def inverse_s_box(output_bits):
     return output
 def p_block(input_bits):
     # P-блок
-    p_indices = [1, 8, 3, 5, 7, 4, 6, 2] 
+    p_indices = [1, 5, 2, 6, 3, 7, 4, 8] 
     output = ''.join(input_bits[i-1] for i in p_indices)
     print(f"P-блок: {input_bits} -> {output}")
     return output
 def inverse_p_block(input_bits):
     # Обратный P-блок
-    inverse_p_indices = [8, 1, 5, 6, 4, 7, 2, 3] 
+    inverse_p_indices = [1, 3, 5, 7, 2, 6, 4, 8] 
     output = ''.join(input_bits[i-1] for i in inverse_p_indices)
     print(f"Обратный P-блок: {input_bits} -> {output}")
     return output
@@ -44,9 +58,9 @@ def xor(bits1, bits2):
     return result_str
 def decrypt(ciphertext):
     # Ключи шифрования
-    k1 = '10101010'
-    k2 = '11111111'
-    k3 = '01010101'
+    k1 = keys[0]
+    k2 = keys[1]
+    k3 = keys[2]
     print(f"Шифртекст: {ciphertext}")
     # Третий цикл
     print("\nШаг 1: Применение третьего цикла")
@@ -67,6 +81,6 @@ def decrypt(ciphertext):
     plaintext = xor(p0, k1)  # p(0) ⊕ k(1)
     return plaintext
 # Пример использования
-ciphertext = '01011000'
+ciphertext = p_c
 plaintext = decrypt(ciphertext)
 print(f"\nОткрытый текст: {plaintext}")
